58097aa453d39ab2f9460a5822719a258ef2b4a1
Redfox <vitor_pereira.2013@hotmail.com>
2021-02-25 22:12:30 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Vitor
+        </h1>
+        <a target="_bank" href="https://github.com/Redfox">Github</a>
+      </main>
+    </div>
+  )
+}
